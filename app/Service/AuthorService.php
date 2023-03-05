a8ceb986c9c2e2578b90e5575d1c4bb54f08b5e8
Ariful Islam <arif98741@gmail.com>
2023-03-06 03:25:32 +0600

    files updated

--- app/Service/AuthorService.php
+++ app/Service/AuthorService.php
@@ -2,7 +2,34 @@
 
 namespace App\Service;
 
+use App\Models\Author;
+
 class AuthorService extends BaseService
 {
     protected $model;
+
+    public function __construct()
+    {
+        $this->model = new Author();
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
 }
